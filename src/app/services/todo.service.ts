3b594c4fd91d3d83de4a8e104aa90aa5bc8c98eb
Ricardo Alzamora <alzamoraricardo5@gmail.com>
2018-03-11 00:34:53 -0500

    Se agrega archivo de datos JSON y agrega petición en el servicio.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { parse } from 'url';
 
 @Injectable()
 export class TodoService {
 
   todoList: AngularFireList<any>;
 
-  constructor(private firebasedb: AngularFireDatabase) { 
-    
+  constructor(private firebasedb: AngularFireDatabase) {
+
   }
 
-  getTodoList(){
-      this.todoList = this.firebasedb.list('titles');
+  getName(code: string) {
+    var request = new XMLHttpRequest();
+    request.open("GET", "../assets/json/candidates.json", false);
+    request.send(null)
+    var my_JSON_object = JSON.parse(request.responseText);
+    return my_JSON_object[code];
   }
 
-  addTodo(title: string){
-    alert("");
+  getTodoList() {
+    this.todoList = this.firebasedb.list('titles');
+  }
+
+  addTodo(title: string) {
+    alert("Votará: " + this.getName(title));
     this.todoList.push({
       title: title,
     })
