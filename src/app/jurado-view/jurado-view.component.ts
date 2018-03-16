54066214dd8dfc1d8c030e693821056c0c0590b8
Ricardo Alzamora <alzamoraricardo5@gmail.com>
2018-03-15 21:36:31 -0500

    Carga de lista de votantes.

--- src/app/jurado-view/jurado-view.component.ts
+++ src/app/jurado-view/jurado-view.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jurado-view.component.css']
 })
 export class JuradoViewComponent implements OnInit {
+  public votantes = this.getVotantes();
+  constructor() {
+   }
 
-  constructor() { }
+   getVotantes(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "../assets/json/authorization.json", false);
+    request.send(null);
+    return JSON.parse(request.responseText);
+   }
+
+   addVotantes(){
+    $.each( this.votantes, function( key, value) {
+      $("ul").append("<li class='list-group-item'>" + key + ": " + value[0]+ " " + value[2] +  "</li>");
+    });
+   }
 
   ngOnInit() {
+    this.addVotantes();
   }
 
 }
